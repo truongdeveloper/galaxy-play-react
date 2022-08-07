a71c9125069f4ea57a36370782f83dcd03e10a09
truong <vanminhtruong678@gmail.com>
2022-08-07 14:08:19 +0700

    commit dev

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Header from "./components/Hearder/Header";
 function App() {
     return (
         <div className="App">
-            <div className="container"></div>
             <Header></Header>
-            <h1>ash</h1>
         </div>
     );
 }
--- src/components/Hearder/Header.jsx
+++ src/components/Hearder/Header.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
-import logo from '../../assets/logoglx.svg';
-import './header.css'
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logoglx.svg";
+import "./header.css";
 
-Header.propTypes = {
-    
-};
+Header.propTypes = {};
 
 function Header(props) {
     return (
         <nav>
-            <NavLink to={''}>
-                <img src={logo} alt="" />
-            </NavLink>
+            <div className="nav__container">
+                <NavLink to={""}>
+                    <img src={logo} alt="" />
+                </NavLink>
+                <div className="menu">
+                    <NavLink to={"/khophim"}>Kho Phim</NavLink>
+                    <NavLink to={"/phim-dien-anh"}>Phim Điện Ảnh</NavLink>
+                    <NavLink to={"/phim-bo"}>Phim Bộ</NavLink>
+                    <NavLink to={"/phim-thue"}>Phim Thuê</NavLink>
+                    <NavLink to={"/khuyen-mai"}>Khuyến Mãi</NavLink>
+                    <NavLink to={"/hotro"}>Hỗ Trợ</NavLink>
+                </div>
+                <div className="login">
+                    <NavLink to={"/login"} className="btn-1">
+                        Đăng Nhập
+                    </NavLink>
+                </div>
+            </div>
         </nav>
     );
 }
