3c9454301f93bf92b777820fd3260e6b6abc5775
truong <vanminhtruong678@gmail.com>
2022-08-28 01:18:17 +0700

    menu-mobile

--- src/components/Hearder/Header.jsx
+++ src/components/Hearder/Header.jsx
@@ -5,6 +5,8 @@ import logo from "../../assets/logoglx.svg";
 import useScroll from "../../hooks/onScroll";
 import "./header.scss";
 import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
+import { isMobile, isMobileOnly } from "react-device-detect";
 
 
 Header.propTypes = {};
@@ -13,23 +15,47 @@ function Header(props) {
 
     const scroll = useScroll();
 
+    const [menu, setMenu] = useState(false);
+    console.log(menu)
+    function handleClick() {
+        if(menu){
+            setMenu(false);
+        }else{
+            setMenu(true);
+        }
+    }
+
     return (
         <nav style={scroll? {background: 'black'}:{}} >
             <div className="nav__container">
-                <div className="menu__mobile">
+                <div className="menu__mobile-icon" onClick={handleClick}>
                     <FaBars className="menu__icon"/>
                 </div>
                 <NavLink to={""}>
                     <img src={logo} alt="" className="img__logo"/>
                 </NavLink>
-                <div className="menu">
-                    <NavLink to={'/'} >Trang chủ</NavLink>
-                    <NavLink to={"/kho-phim"}>Kho Phim</NavLink>
-                    <NavLink to={"/phim-dien-anh?gener=movie-now"}>Phim Điện Ảnh</NavLink>
-                    <NavLink to={"/phim-bo"}>Phim Bộ</NavLink>
-                    <NavLink to={"/phim-thue"}>Phim Thuê</NavLink>
-                    <NavLink to={"/khuyen-mai"}>Khuyến Mãi</NavLink>
-                    <NavLink to={"/ho-tro"}>Hỗ Trợ</NavLink>
+                {(isMobile && !menu)?
+                        null
+                    :
+                    <div className="menu__mobile" onClick={handleClick}>
+                        <NavLink to={'/'} data-aos="fade-up">Trang chủ</NavLink>
+                        <NavLink to={"/kho-phim"} data-aos="fade-up">Kho Phim</NavLink>
+                        <NavLink to={"/phim-dien-anh?gener=movie-now"} data-aos="fade-up">Phim Điện Ảnh</NavLink>
+                        <NavLink to={"/phim-bo"} data-aos="fade-up">Phim Bộ</NavLink>
+                        <NavLink to={"/phim-thue"} data-aos="fade-up">Phim Thuê</NavLink>
+                        <NavLink to={"/khuyen-mai"} data-aos="fade-up">Khuyến Mãi</NavLink>
+                        <NavLink to={"/ho-tro"} data-aos="fade-up">Hỗ Trợ</NavLink>
+                    </div>
+                    
+                }
+                <div className="menu" onClick={handleClick}>
+                    <NavLink to={'/'} data-aos="fade-up">Trang chủ</NavLink>
+                    <NavLink to={"/kho-phim"} data-aos="fade-up">Kho Phim</NavLink>
+                    <NavLink to={"/phim-dien-anh?gener=movie-now"} data-aos="fade-up">Phim Điện Ảnh</NavLink>
+                    <NavLink to={"/phim-bo"} data-aos="fade-up">Phim Bộ</NavLink>
+                    <NavLink to={"/phim-thue"} data-aos="fade-up">Phim Thuê</NavLink>
+                    <NavLink to={"/khuyen-mai"} data-aos="fade-up">Khuyến Mãi</NavLink>
+                    <NavLink to={"/ho-tro"} data-aos="fade-up">Hỗ Trợ</NavLink>
                 </div>
                 <div className="search__area" >
                     <input type="text" className="search" placeholder="Name, Gener, Actor, ..."/>
